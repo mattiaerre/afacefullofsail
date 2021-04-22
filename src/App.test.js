604486ffd089d06a61f85fb12564436b2f7afb78
mattia richetto <mattia.richetto@gmail.com>
2021-04-21 20:47:39 -0700

    mock version

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,11 @@ import App from './App';
 
 jest.mock('react-uuid', () => () => '0000000-000-0000-000-00000000000');
 
+jest.mock('../package.json', () => ({
+  ...jest.requireActual('../package.json'),
+  version: '0.0.0-jest.0'
+}));
+
 test('App snapshot', () => {
   const { container } = render(<App />);
   expect(container).toMatchSnapshot();
