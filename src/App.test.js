b8af8714efe75d6e08e39882221361d7fa8388e8
Mattia Richetto <mattia.richetto@gmail.com>
2022-08-20 17:53:10 -0700

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,18 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { name } from './info';
 
 jest.mock('react-uuid', () => () => '0000000-000-0000-000-00000000000');
 
-jest.mock('../package.json', () => ({
-  ...jest.requireActual('../package.json'),
-  version: '0.0.0-jest.0'
-}));
+const version = '0.0.0-jest.0';
 
 test('App snapshot', () => {
-  const { container } = render(<App />);
+  const { container } = render(<App name={name} version={version} />);
   expect(container).toMatchSnapshot();
 });
 
 test('same vessel type', () => {
-  render(<App />);
+  render(<App name={name} version={version} />);
 
   fireEvent.click(screen.getAllByTestId('radio')[5]); // Vessel B, type: BIG_SHIP
 
